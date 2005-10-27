44a9447cb210081c69de88403f707b45f3ff9c7c
Philippe Kernevez <pkernevez@github.com>
2005-10-27 22:28:01 +0000

    Refactoring

---
+++ core_monitoring/src-java/org/jmonitoring/core/store/IStoreReader.java
@@ -0,0 +1,17 @@
+package org.jmonitoring.core.store;
+
+/***************************************************************************
+ * Copyright 2005 Philippe Kernevez All rights reserved.                   *
+ * Please look at license.txt for more license detail.                     *
+ **************************************************************************/
+
+/**
+ * @author pke
+ *
+ * @todo To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public interface IStoreReader
+{
+
+}
