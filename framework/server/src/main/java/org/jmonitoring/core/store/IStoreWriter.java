65276b661c7bb93b72761ab6f9279918f01aac13
Philippe Kernevez <pkernevez@github.com>
2007-01-22 00:02:36 +0000

    Refactoring for the creation of the server and agent module

---
+++ framework/server/src/main/java/org/jmonitoring/core/store/IStoreWriter.java
@@ -0,0 +1,25 @@
+package org.jmonitoring.core.store;
+
+import org.jmonitoring.core.persistence.ExecutionFlowPO;
+
+/***************************************************************************
+ * Copyright 2005 Philippe Kernevez All rights reserved.                   *
+ * Please look at license.txt for more license detail.                     *
+ **************************************************************************/
+
+/**
+ * @author pke
+ * 
+ * @todo To change the template for this generated type comment go to Window - Preferences - Java - Code Style - Code
+ *       Templates
+ */
+public interface IStoreWriter
+{
+    /**
+     * Write the current MeasurePointTree in a store.
+     * 
+     * @param pExecutionFlow The flow to insert into the store.
+     */
+    void writeExecutionFlow(ExecutionFlowPO pExecutionFlow);
+
+}
