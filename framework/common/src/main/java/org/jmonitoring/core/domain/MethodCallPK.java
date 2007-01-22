abb58dd85c3b578bfd43edf4cf833c9137c4352b
Philippe Kernevez <pkernevez@github.com>
2007-01-22 00:39:01 +0000

    refactoring

---
+++ framework/common/src/main/java/org/jmonitoring/core/domain/MethodCallPK.java
@@ -0,0 +1,80 @@
+package org.jmonitoring.core.domain;
+
+
+import java.io.Serializable;
+
+public class MethodCallPK implements Serializable
+{
+
+    private static final long serialVersionUID = -329582789701661686L;
+
+    private ExecutionFlowPO mFlow;
+
+    private int mPosition;
+
+    public MethodCallPK()
+    {
+    }
+
+    public MethodCallPK(ExecutionFlowPO pFlow, int pPosition)
+    {
+        super();
+        mFlow = pFlow;
+        mPosition = pPosition;
+    }
+
+    public ExecutionFlowPO getFlow()
+    {
+        return mFlow;
+    }
+
+    public void setFlow(ExecutionFlowPO pFlow)
+    {
+        mFlow = pFlow;
+    }
+
+    public int getPosition()
+    {
+        return mPosition;
+    }
+
+    public void setPosition(int pPosition)
+    {
+        mPosition = pPosition;
+    }
+
+    public int hashCode()
+    {
+        final int PRIME = 31;
+        int result = 1;
+        result = PRIME * result + ((mFlow == null) ? 0 : mFlow.hashCode());
+        result = PRIME * result + mPosition;
+        return result;
+    }
+
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        final MethodCallPK other = (MethodCallPK) obj;
+        if (mFlow == null)
+        {
+            if (other.mFlow != null)
+                return false;
+        } else if (!mFlow.equals(other.mFlow))
+            return false;
+        if (mPosition != other.mPosition)
+            return false;
+        return true;
+    }
+
+    public String toString()
+    {
+        return "FlowId=[" + mFlow.getId() + "] et IndexInFlow=[" + mPosition + "]";
+    }
+
+}
