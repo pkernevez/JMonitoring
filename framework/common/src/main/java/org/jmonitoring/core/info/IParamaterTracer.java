aa8e4f0a32b755fdbc79a7cf25e0e1ddb6ec413d
Philippe Kernevez <pkernevez@github.com>
2007-01-23 00:04:59 +0000

    Refactoring for the creation of the server and agent module

---
+++ framework/common/src/main/java/org/jmonitoring/core/info/IParamaterTracer.java
@@ -0,0 +1,14 @@
+package org.jmonitoring.core.info;
+
+public interface IParamaterTracer
+{
+    /**
+     * Convert an object array to String.
+     * @param pTarget The taget instance on which the Method was called.
+     * @param pParameterObjects The objects passed to the intercepted method.
+     * 
+     * @return The objects as String.
+     */
+    String convertToString(Object pTarget, Object[] pParameterObjects);
+
+}
