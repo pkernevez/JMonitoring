e68630ec20877adff7dd8121ab8e07a328a7d09f
Philippe Kernevez <pkernevez@github.com>
2009-03-06 19:54:21 +0000

    Trace size reduction

--- framework/extensions/agent-sql/src/main/java/org/jmonitoring/agent/sql/JMonitoringPreparedStatement.java
+++ framework/extensions/agent-sql/src/main/java/org/jmonitoring/agent/sql/JMonitoringPreparedStatement.java
@@ -174,195 +174,188 @@ public class JMonitoringPreparedStatement extends JMonitoringStatement implement
 
     public void setArray(int pParameterIndex, Array pX) throws SQLException
     {
-        mTrace.append("Add ARRAY parameter, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
+        mTrace.append("Add ARRAY, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
         mRealPreparedStat.setArray(pParameterIndex, pX);
     }
 
     public void setAsciiStream(int pParameterIndex, InputStream pX, int pLength) throws SQLException
     {
-        mTrace.append("Add AsciiStream parameter, pos=[" + pParameterIndex + "], value not traced, length=[" + pLength
-            + "]\n");
+        mTrace.append("Add AsciiStream, pos=[" + pParameterIndex + "], value not traced, length=[" + pLength + "]\n");
         mRealPreparedStat.setAsciiStream(pParameterIndex, pX, pLength);
     }
 
     public void setBigDecimal(int pParameterIndex, BigDecimal pX) throws SQLException
     {
-        mTrace.append("Add BigDecimal parameter, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
+        mTrace.append("Add BigDecimal, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
         mRealPreparedStat.setBigDecimal(pParameterIndex, pX);
     }
 
     public void setBinaryStream(int pParameterIndex, InputStream pX, int pLength) throws SQLException
     {
-        mTrace.append("Add BinaryStream parameter, pos=[" + pParameterIndex + "], value not traced, length=[" + pLength
-            + "]\n");
+        mTrace.append("Add BinaryStream, pos=[" + pParameterIndex + "], value not traced, length=[" + pLength + "]\n");
         mRealPreparedStat.setBinaryStream(pParameterIndex, pX, pLength);
     }
 
     public void setBlob(int pI, Blob pX) throws SQLException
     {
-        mTrace.append("Add Blob parameter, pos=[" + pI + "], value not traced");
+        mTrace.append("Add Blob, pos=[" + pI + "], value not traced");
         mRealPreparedStat.setBlob(pI, pX);
 
     }
 
     public void setBoolean(int pParameterIndex, boolean pX) throws SQLException
     {
-        mTrace.append("Add Boolean parameter, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
+        mTrace.append("Add Boolean, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
         mRealPreparedStat.setBoolean(pParameterIndex, pX);
     }
 
     public void setByte(int pParameterIndex, byte pX) throws SQLException
     {
-        mTrace.append("Add Byte parameter, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
+        mTrace.append("Add Byte, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
         mRealPreparedStat.setByte(pParameterIndex, pX);
     }
 
     public void setBytes(int pPrameterIndex, byte[] pX) throws SQLException
     {
-        mTrace.append("Add Bytes parameter, pos=[" + pPrameterIndex + "], value=[" + pX + "]\n");
+        mTrace.append("Add Bytes, pos=[" + pPrameterIndex + "], value=[" + pX + "]\n");
         mRealPreparedStat.setBytes(pPrameterIndex, pX);
     }
 
     public void setCharacterStream(int pParameterIndex, Reader pReader, int pLength) throws SQLException
     {
-        mTrace.append("Add CharacterStream parameter, pos=[" + pParameterIndex + "], value not traced, length=["
-            + pLength + "]");
+        mTrace.append("Add CharacterStream, pos=[" + pParameterIndex + "], value not traced, length=[" + pLength + "]");
         mRealPreparedStat.setCharacterStream(pParameterIndex, pReader, pLength);
 
     }
 
     public void setClob(int pI, Clob pX) throws SQLException
     {
-        mTrace.append("Add Clob parameter, pos=[" + pI + "], value not traced");
+        mTrace.append("Add Clob, pos=[" + pI + "], value not traced");
         mRealPreparedStat.setClob(pI, pX);
     }
 
     public void setDate(int pParameterIndex, Date pX) throws SQLException
     {
-        mTrace.append("Add Date parameter, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
+        mTrace.append("Add Date, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
         mRealPreparedStat.setDate(pParameterIndex, pX);
     }
 
     public void setDate(int pParameterIndex, Date pX, Calendar pCal) throws SQLException
     {
-        mTrace
-              .append("Add Date parameter, pos=[" + pParameterIndex + "], value=[" + pX + ", calendar=[" + pCal + "]\n");
+        mTrace.append("Add Date, pos=[" + pParameterIndex + "], value=[" + pX + ", calendar=[" + pCal + "]\n");
         mRealPreparedStat.setDate(pParameterIndex, pX, pCal);
     }
 
     public void setDouble(int pParameterIndex, double pX) throws SQLException
     {
-        mTrace.append("Add Double parameter, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
+        mTrace.append("Add Double, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
         mRealPreparedStat.setDouble(pParameterIndex, pX);
     }
 
     public void setFloat(int pParameterIndex, float pX) throws SQLException
     {
-        mTrace.append("Add Float parameter, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
+        mTrace.append("Add Float, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
         mRealPreparedStat.setFloat(pParameterIndex, pX);
     }
 
     public void setInt(int pParameterIndex, int pX) throws SQLException
     {
-        mTrace.append("Add Int parameter, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
+        mTrace.append("Add Int, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
         mRealPreparedStat.setInt(pParameterIndex, pX);
     }
 
     public void setLong(int pParameterIndex, long pX) throws SQLException
     {
-        mTrace.append("Add Long parameter, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
+        mTrace.append("Add Long, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
         mRealPreparedStat.setLong(pParameterIndex, pX);
     }
 
     public void setNull(int pParameterIndex, int pSqlType) throws SQLException
     {
-        mTrace.append("Add NULL parameter, pos=[" + pParameterIndex + "], type=[" + pSqlType + "]\n");
+        mTrace.append("Add NULL, pos=[" + pParameterIndex + "], type=[" + pSqlType + "]\n");
         mRealPreparedStat.setNull(pParameterIndex, pSqlType);
     }
 
     public void setNull(int pParameterIndex, int pSqlType, String pTypeName) throws SQLException
     {
-        mTrace.append("Add NULL parameter, pos=[" + pParameterIndex + "], type=[" + pSqlType + "], type name=["
-            + pTypeName + "]\n");
+        mTrace.append("Add NULL, pos=[" + pParameterIndex + "], type=[" + pSqlType + "], type name=[" + pTypeName
+            + "]\n");
         mRealPreparedStat.setNull(pParameterIndex, pSqlType, pTypeName);
     }
 
     public void setObject(int pParameterIndex, Object pX) throws SQLException
     {
-        mTrace.append("Add Object parameter, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
+        mTrace.append("Add Object, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
         mRealPreparedStat.setObject(pParameterIndex, pX);
     }
 
     public void setObject(int pParameterIndex, Object pX, int pTargetSqlType) throws SQLException
     {
-        mTrace.append("Add Object parameter, pos=[" + pParameterIndex + "], value=[" + pX + "], targetTyp=["
-            + pTargetSqlType + "]\n");
+        mTrace.append("Add Object, pos=[" + pParameterIndex + "], value=[" + pX + "], targetTyp=[" + pTargetSqlType
+            + "]\n");
         mRealPreparedStat.setObject(pParameterIndex, pX, pTargetSqlType);
     }
 
     public void setObject(int pParameterIndex, Object pX, int pTargetSqlType, int pScale) throws SQLException
     {
-        mTrace.append("Add Object parameter, pos=[" + pParameterIndex + "], value=[" + pX + "], targetTyp=["
-            + pTargetSqlType + "], scale=[" + pScale + "]\n");
+        mTrace.append("Add Object, pos=[" + pParameterIndex + "], value=[" + pX + "], targetTyp=[" + pTargetSqlType
+            + "], scale=[" + pScale + "]\n");
         mRealPreparedStat.setObject(pParameterIndex, pX, pTargetSqlType, pScale);
     }
 
     public void setRef(int pParameterIndex, Ref pX) throws SQLException
     {
-        mTrace.append("Add Ref parameter, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
+        mTrace.append("Add Ref, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
         mRealPreparedStat.setRef(pParameterIndex, pX);
     }
 
     public void setShort(int pParameterIndex, short pX) throws SQLException
     {
-        mTrace.append("Add Short parameter, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
+        mTrace.append("Add Short, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
         mRealPreparedStat.setShort(pParameterIndex, pX);
 
     }
 
     public void setString(int pParameterIndex, String pX) throws SQLException
     {
-        mTrace.append("Add String parameter, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
+        mTrace.append("Add String, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
         mRealPreparedStat.setString(pParameterIndex, pX);
     }
 
     public void setTime(int pParameterIndex, Time pX) throws SQLException
     {
-        mTrace.append("Add Time parameter, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
+        mTrace.append("Add Time, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
         mRealPreparedStat.setTime(pParameterIndex, pX);
     }
 
     public void setTime(int pParameterIndex, Time pX, Calendar pCal) throws SQLException
     {
-        mTrace.append("Add Time parameter, pos=[" + pParameterIndex + "], value=[" + pX + "], Calendar=[" + pCal
-            + "]\n");
+        mTrace.append("Add Time, pos=[" + pParameterIndex + "], value=[" + pX + "], Calendar=[" + pCal + "]\n");
         mRealPreparedStat.setTime(pParameterIndex, pX, pCal);
     }
 
     public void setTimestamp(int pParameterIndex, Timestamp pX) throws SQLException
     {
-        mTrace.append("Add Timestamp parameter, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
+        mTrace.append("Add Timestamp, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
         mRealPreparedStat.setTimestamp(pParameterIndex, pX);
     }
 
     public void setTimestamp(int pParameterIndex, Timestamp pX, Calendar pCal) throws SQLException
     {
-        mTrace.append("Add Timestamp parameter, pos=[" + pParameterIndex + "], value=[" + pX + "], Calendar=[" + pCal
-            + "]\n");
+        mTrace.append("Add Timestamp, pos=[" + pParameterIndex + "], value=[" + pX + "], Calendar=[" + pCal + "]\n");
         mRealPreparedStat.setTimestamp(pParameterIndex, pX, pCal);
     }
 
     public void setURL(int pParameterIndex, URL pX) throws SQLException
     {
-        mTrace.append("Add URL parameter, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
+        mTrace.append("Add URL, pos=[" + pParameterIndex + "], value=[" + pX + "]\n");
         mRealPreparedStat.setURL(pParameterIndex, pX);
     }
 
     @SuppressWarnings("deprecation")
     public void setUnicodeStream(int pParameterIndex, InputStream pX, int pLength) throws SQLException
     {
-        mTrace.append("Add UnicodeStream parameter, pos=[" + pParameterIndex + "], value not traced, length=["
-            + pLength + "]\n");
+        mTrace.append("Add UnicodeStream, pos=[" + pParameterIndex + "], value not traced, length=[" + pLength + "]\n");
         mRealPreparedStat.setUnicodeStream(pParameterIndex, pX, pLength);
     }
 
