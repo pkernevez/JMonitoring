87e391fc753b48bd05c130a18fdf2c536f31932c
Philippe Kernevez <pkernevez@github.com>
2011-07-29 10:42:42 +0000

    Pour corriger le build

--- framework/common/src/test/java/org/jmonitoring/core/configuration/FormaterBeanTest.java
+++ framework/common/src/test/java/org/jmonitoring/core/configuration/FormaterBeanTest.java
@@ -1,6 +1,9 @@
 package org.jmonitoring.core.configuration;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 import javax.annotation.Resource;
 
@@ -26,18 +29,35 @@ public class FormaterBeanTest extends TestCase
      * Get the jet lag between the local of the test author and the runner.
      * 
      * @return
+     * @throws ParseException
      */
-//    private long getElapseTime(){
-//        long 
-//          }
+    private long getElapseTime() throws ParseException
+    {
+        Locale tAuthorLocal = new Locale("fr", "CH");
+        Locale tDefaultLocal = Locale.getDefault();
+        SimpleDateFormat tAuthorFormatter = new SimpleDateFormat("26/04/70 18:46:40", tAuthorLocal);
+        SimpleDateFormat tDefaultFormatter = new SimpleDateFormat("26/04/70 18:46:40", tDefaultLocal);
+        long tResult =
+            tDefaultFormatter.parse("26/04/70 18:46:40").getTime()
+                - tAuthorFormatter.parse("26/04/70 18:46:40").getTime();
+        return tResult;
+    }
 
     @Test
-    public void testFormatDateTime()
+    public void testElapseTime() throws ParseException
+    {
+        assertEquals(0, getElapseTime());
+    }
+
+    @Test
+    public void testFormatDateTime() throws ParseException
     {
         mFormater.setDateFormat("dd/MM/yy");
         mFormater.setTimeFormat("HH:mm:ss");
-        assertEquals("26/04/70 18:46:40", mFormater.formatDateTime(10000000000L));
-        assertEquals("26/04/70 18:46:40", mFormater.formatDateTime(new Date(10000000000L)));
+        assertEquals(Locale.getDefault().toString(), "26/04/70 18:46:40",
+                     mFormater.formatDateTime(10000000000L + getElapseTime()));
+        assertEquals(Locale.getDefault().toString(), "26/04/70 18:46:40",
+                     mFormater.formatDateTime(new Date(10000000000L + getElapseTime())));
         mFormater.setDateFormat("yy/MM/dd");
         mFormater.setTimeFormat("HH:mm");
         assertEquals("70/04/26 18:46", mFormater.formatDateTime(10000000000L));
