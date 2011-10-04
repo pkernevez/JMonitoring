dba62c65091e5cdf294531024ebdce3234d556f2
Kernevez <pkernevez@octo.com>
2011-10-04 22:17:41 +0200

    Add transaction support

--- framework/console/console-gwt2/src/main/java/org/jmonitoring/console/gwt/server/flow/TransactionFilter.java
+++ framework/console/console-gwt2/src/main/java/org/jmonitoring/console/gwt/server/flow/TransactionFilter.java
@@ -9,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -21,10 +22,11 @@ import org.springframework.web.context.support.WebApplicationContextUtils;
 public class TransactionFilter implements Filter
 {
     SessionFactory sessionFactory;
-    
+
     public void init(FilterConfig pConfig) throws ServletException
     {
-        WebApplicationContext springContext = WebApplicationContextUtils.getWebApplicationContext(pConfig.getServletContext());
+        WebApplicationContext springContext =
+            WebApplicationContextUtils.getWebApplicationContext(pConfig.getServletContext());
         sessionFactory = (SessionFactory) springContext.getBean(SessionFactory.class);
     }
 
@@ -32,14 +34,61 @@ public class TransactionFilter implements Filter
         ServletException
     {
         Session tSession = sessionFactory.openSession();
-        tSession.beginTransaction();
+        Transaction tCurrentTx = tSession.beginTransaction();
         TransactionSynchronizationManager.bindResource(sessionFactory, new SessionHolder(tSession));
-        pChain.doFilter(pRequest, pResponse);
-        HttpServletResponse tResponse = (HttpServletResponse) pResponse;
-//        if (tResponse.)
+        HttpServletResponseWithStatus tResponse = new HttpServletResponseWithStatus((HttpServletResponse) pResponse);
+        pChain.doFilter(pRequest, tResponse);
+        if (tResponse.httpStatus == 200){
+            tCurrentTx.commit();
+        } else {
+            tCurrentTx.rollback();
+        }
+        tSession.close();
+        TransactionSynchronizationManager.unbindResource(sessionFactory);
     }
 
     public void destroy()
     {
     }
+
+    public static class HttpServletResponseWithStatus extends HttpServletResponseWrapper
+    {
+        private int httpStatus = 200;
+
+        public HttpServletResponseWithStatus(HttpServletResponse pResponse)
+        {
+            super(pResponse);
+        }
+
+        @Override
+        public void sendError(int sc) throws IOException {
+            httpStatus = sc;
+            super.sendError(sc);
+        }
+
+        @Override
+        public void sendError(int sc, String msg) throws IOException {
+            httpStatus = sc;
+            super.sendError(sc, msg);
+        }
+
+
+        @Override
+        public void setStatus(int sc) {
+            httpStatus = sc;
+            super.setStatus(sc);
+        }
+
+        public int getStatus() {
+            return httpStatus;
+        }
+        
+        @Override
+        public void sendRedirect(String location) throws IOException {
+            httpStatus = 302;
+            super.sendRedirect(location);
+        }
+
+    }
+
 }
